046a6e0e43632a846cc873e90d8b15272d826ba2
Kevin Clyne <kevin@clyne.com>
2022-02-25 15:54:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,17 +17,17 @@ with form:
     submitted = st.form_submit_button(label="Submit")
 
 
-m = folium.Map(location=[40.70, -73.94], zoom_start=10, tiles='CartoDB positron')
-Draw(export=False).add_to(m)
+    m = folium.Map(location=[40.70, -73.94], zoom_start=10, tiles='CartoDB positron')
+    Draw(export=False).add_to(m)
 
-if uploaded_file is not None:
-    dataframe = gpd.read_file(uploaded_file).to_crs(epsg=4326)
-    geometry = dataframe['geometry']
-    bbox = dataframe.total_bounds
-    m.fit_bounds([[bbox[1],bbox[0]],[bbox[3],bbox[2]]], padding=[20,20])
-    folium.GeoJson(data=geometry).add_to(m)
+    if uploaded_file is not None:
+        dataframe = gpd.read_file(uploaded_file).to_crs(epsg=4326)
+        geometry = dataframe['geometry']
+        bbox = dataframe.total_bounds
+        m.fit_bounds([[bbox[1],bbox[0]],[bbox[3],bbox[2]]], padding=[20,20])
+        folium.GeoJson(data=geometry).add_to(m)
 
 
-# call to render Folium map in Streamlit
-folium_static(m)
+    # call to render Folium map in Streamlit
+    folium_static(m)
 
