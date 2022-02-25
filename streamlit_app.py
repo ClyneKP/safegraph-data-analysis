0398533e633ce798ec971d79d195374d5f860e69
Kevin Clyne <kevin@clyne.com>
2022-02-25 12:56:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,31 @@
 import streamlit as st
-import geopandas as gpd
-import numpy as np
-import shapestats_kc as shp
-import matplotlib.pyplot as plt
-import folium
-from folium.plugins import Draw
-from streamlit_folium import st_folium
+from streamlit_folium import folium_static
+import geemap.eefolium as geemap
 import ee
+
+# os.environ["EARTHENGINE_TOKEN"] == st.secrets["EARTHENGINE_TOKEN"]
 service_account = "admin-815@earth-engine-342417.iam.gserviceaccount.com"
 credentials = ee.ServiceAccountCredentials(service_account, 'earth-engine-342417-f19485aba489.json')
 ee.Initialize(credentials)
-import geemap
+"# streamlit geemap demo"
+st.markdown('Source code: <https://github.com/giswqs/geemap-streamlit/blob/main/geemap_app.py>')
 
-st.set_page_config(page_title="streamlit-folium documentation")
+with st.echo():
+    import streamlit as st
+    from streamlit_folium import folium_static
+    import geemap.eefolium as geemap
+    import ee
 
-"# streamlit-folium"
+    m = geemap.Map()
+    dem = ee.Image('USGS/SRTMGL1_003')
 
-data = gpd.read_file("Lower Manhattan2.geojson").to_crs(epsg=26914)
+    vis_params = {
+    'min': 0,
+    'max': 4000,
+    'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
 
+    m.addLayer(dem, vis_params, 'SRTM DEM', True, 1)
+    m.addLayerControl()
 
-
-import streamlit as st
-from streamlit_folium import folium_static
-import folium
-
-uploaded_file = st.file_uploader("Upload Study Area Shapefile")
-
-
-#m = folium.Map(location=[40.70, -73.94], zoom_start=10, tiles='CartoDB positron')
-m = geemap.Map(center=[40.70, -73.94], zoom=4)
-#Draw(export=False).add_to(m)
-
-if uploaded_file is not None:
-    dataframe = gpd.read_file(uploaded_file).to_crs(epsg=4326)
-    geometry = dataframe['geometry']
-    bbox = dataframe.total_bounds
-    m.fit_bounds([[bbox[1],bbox[0]],[bbox[3],bbox[2]]], padding=[20,20])
-    folium.GeoJson(data=geometry).add_to(m)
-
-
-# call to render Folium map in Streamlit
-folium_static(m)
-
+    # call to render Folium map in Streamlit
+    folium_static(m)
