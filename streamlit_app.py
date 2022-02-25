b0240caf4c6aef10c8dfdb2aaaa2a6a1d71e1aae
Kevin Clyne <kevin@clyne.com>
2022-02-25 13:23:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,25 +10,28 @@ service_account = "admin-815@earth-engine-342417.iam.gserviceaccount.com"
 credentials = ee.ServiceAccountCredentials(service_account, 'earth-engine-342417-f19485aba489.json')
 ee.Initialize(credentials)
 "# streamlit geemap demo"
-st.markdown('Source code: <https://github.com/giswqs/geemap-streamlit/blob/main/geemap_app.py>')
 
-with st.echo():
-    import streamlit as st
-    from streamlit_folium import folium_static
-    import geemap.eefolium as geemap
-    import ee
+import streamlit as st
+from streamlit_folium import folium_static
+import geemap.eefolium as geemap
+import ee
 
-    m = geemap.Map(center=[40.70, -73.94], zoom=4)
-    Draw(export=False).add_to(m)
-    dem = ee.Image('USGS/SRTMGL1_003')
+m = geemap.Map(center=[40.70, -73.94], zoom=4)
+Draw(export=False).add_to(m)
+dem = ee.Image('USGS/SRTMGL1_003')
 
-    vis_params = {
-    'min': 0,
-    'max': 4000,
-    'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
+vis_params = {
+'min': 0,
+'max': 4000,
+'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
 
-    m.addLayer(dem, vis_params, 'SRTM DEM', True, 1)
-    m.addLayerControl()
+m.addLayer(dem, vis_params, 'SRTM DEM', True, 1)
+m.addLayerControl()
 
-    # call to render Folium map in Streamlit
-    folium_static(m)
+# call to render Folium map in Streamlit
+folium_static(m)
+
+def get_features():
+    st.write(m.draw_features)
+
+st.button("Get Features",on_click=get_features())
