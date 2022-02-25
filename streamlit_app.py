1d419d818a66d2f628266a80b7dae3ba4ae7a60f
Kevin Clyne <kevin@clyne.com>
2022-02-25 11:32:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,27 +10,26 @@ st.set_page_config(page_title="streamlit-folium documentation")
 
 data = gpd.read_file("Lower Manhattan2.geojson").to_crs(epsg=26914)
 
-with st.echo():
-
-    import streamlit as st
-    from streamlit_folium import folium_static
-    import folium
-
-    uploaded_file = st.file_uploader("Upload Study Area Shapefile")
-
-    if uploaded_file is None:
-        m = folium.Map(location=[40.70, -73.94], zoom_start=10, tiles='CartoDB positron')
-        folium.GeoJson(data=data['geometry']).add_to(m)
-    else:
-        dataframe = gpd.read_file(uploaded_file).to_crs(epsg=4326)
-        geometry = dataframe['geometry']
-        bbox = dataframe.total_bounds
-        st.write(bbox)
-        m = folium.Map(location=[40.70, -73.94], tiles='CartoDB positron')
-        m.fit_bounds([[bbox[1],bbox[0]],[bbox[3],bbox[2]]], padding=[20,20])
-        folium.GeoJson(data=geometry).add_to(m)
 
 
-    # call to render Folium map in Streamlit
-    folium_static(m)
+import streamlit as st
+from streamlit_folium import folium_static
+import folium
+
+uploaded_file = st.file_uploader("Upload Study Area Shapefile")
+
+
+m = folium.Map(location=[40.70, -73.94], zoom_start=10, tiles='CartoDB positron')
+Draw(export=False).add_to(m)
+
+if uploaded_file is not None:
+    dataframe = gpd.read_file(uploaded_file).to_crs(epsg=4326)
+    geometry = dataframe['geometry']
+    bbox = dataframe.total_bounds
+    m.fit_bounds([[bbox[1],bbox[0]],[bbox[3],bbox[2]]], padding=[20,20])
+    folium.GeoJson(data=geometry).add_to(m)
+
+
+# call to render Folium map in Streamlit
+folium_static(m)
 
